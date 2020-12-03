7544b705100265e1dfaad2df7911ce1af286dba3
Thomas Nelson <tnelson@flowmill.com>
2020-12-02 17:42:54 -0800

    Add labels to the metrics with the names of tests
    I wanted to make a Grafana dashboard that would display the failing
    tests, but realized I didn't have a good metric to query that contained
    that information.
    
    Now instead of writing a metric per test, this will add 'testname'
    labels to the aggregate metrics of passed, failed, and skipped

--- pytest_prometheus/__init__.py
+++ pytest_prometheus/__init__.py
@@ -43,9 +43,9 @@ class PrometheusReport:
         self.job_name = config.getoption('prometheus_job_name')
         self.registry = CollectorRegistry()
 
-        self.passed = 0
-        self.failed = 0
-        self.skipped = 0
+        self.passed = []
+        self.failed = []
+        self.skipped = []
 
         self.extra_labels = {item[0]: item[1] for item in [i.split('=', 1) for i in config.getoption('prometheus_extra_label')]}
 
@@ -60,52 +60,57 @@ class PrometheusReport:
         replacement = '_'
         return re.sub(pattern, replacement, unsanitized_name)
 
+    def _make_labels(self, testname):
+        ret = self.extra_labels.copy()
+        ret["testname"] = testname
+        return ret
+
+    def _get_label_names(self):
+        return self._make_labels("").keys()
+
+    def add_metrics_for_tests(self, metric, testnames):
+        for testname in testnames:
+            labels = self._make_labels(testname)
+            metric.labels(**labels).inc()
+
+
     def pytest_runtest_logreport(self, report):
         # https://docs.pytest.org/en/latest/reference.html#_pytest.runner.TestReport.when
         # 'call' is the phase when the test is being ran
         if report.when == 'call':
 
-            metric_value = 0
-
-            if report.outcome == 'passed':
-                self.passed += 1
-                metric_value = 1
-            elif report.outcome == 'skipped':
-                self.skipped += 1
-            elif report.outcome == 'failed':
-                self.failed += 1
-
-
             funcname = report.location[2]
             name = self._make_metric_name(funcname)
-            logging.debug("Creating metric {name}".format(name=name))
-            metric = Gauge(name,
-                    report.nodeid,
-                    labelnames=self.extra_labels.keys(),
-                    registry=self.registry)
-            # You can't just call metric.set(), you have to call metric.labels() first and include
-            # all the labels declared as `labelnames` in the constructor
-            metric.labels(**self.extra_labels).set(metric_value)
+
+            if report.outcome == 'passed':
+                self.passed.append(name)
+            elif report.outcome == 'skipped':
+                self.skipped.append(name)
+            elif report.outcome == 'failed':
+                self.failed.append(name)
+
+
 
     def pytest_sessionfinish(self, session):
 
         passed_metric = Gauge(self._make_metric_name("passed"),
                 "Number of passed tests",
-                labelnames=self.extra_labels.keys(),
+                labelnames=self._get_label_names(),
                 registry=self.registry)
-        passed_metric.labels(**self.extra_labels).set(self.passed)
+        self.add_metrics_for_tests(passed_metric, self.passed)
+
 
         failed_metric = Gauge(self._make_metric_name("failed"),
                 "Number of failed tests",
-                labelnames=self.extra_labels.keys(),
+                labelnames=self._get_label_names(),
                 registry=self.registry)
-        failed_metric.labels(**self.extra_labels).set(self.failed)
+        self.add_metrics_for_tests(failed_metric, self.failed)
 
         skipped_metric = Gauge(self._make_metric_name("skipped"),
                 "Number of skipped tests",
-                labelnames=self.extra_labels.keys(),
+                labelnames=self._get_label_names(),
                 registry=self.registry)
-        skipped_metric.labels(**self.extra_labels).set(self.skipped)
+        self.add_metrics_for_tests(skipped_metric, self.skipped)
 
         push_to_gateway(self.pushgateway_url, registry=self.registry, job=self.job_name)
 
