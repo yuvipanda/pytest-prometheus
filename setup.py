0752d372c6dae8cfe452fa8bc8a3a23b48efc017
Thomas Nelson <tnelson@flowmill.com>
2020-12-02 17:55:03 -0800

    Update version to 0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pytest-prometheus',
-    version='0.3',
+    version='0.4',
     description='Report test pass / failures to a Prometheus PushGateway',
     author='Yuvi Panda',
     author_email='yuvipanda@gmail.com',
